1332bb7169e675017329ee953969bdd82271ac59
loganbyeon <113130701+loganbyeon@users.noreply.github.com>
2022-12-15 20:46:53 -0800

    Testing LeapYear & InputHelper classes

--- src/main/java/u4pp/Main.java
+++ src/main/java/u4pp/Main.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("welcome to u4pp! Good luck :)\n"+
-        "(Please delete this line of code before submitting :) )");
+        Scanner input = new Scanner(System.in);
+        // test for isLeapYear
+        // int year = input.nextInt();
+        // System.out.println(LeapYear.isLeapYear(year));
+
+        InputHelper h = new InputHelper(input);
+        // test for getYesNoInput
+        // System.out.println(h.getYesNoInput("yes or no?"));
+        
+        // test for getIntegerInput
+        System.out.println(h.getIntegerInput("yes or no?", 0, 5));
     }
 }
