8243494eb7dd7f0e81cefe098e75e3ba483874d1
loganbyeon <113130701+loganbyeon@users.noreply.github.com>
2022-12-15 21:14:50 -0800

    Adding Java Doc for InputHelper

--- src/main/java/u4pp/InputHelper.java
+++ src/main/java/u4pp/InputHelper.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 public class InputHelper {
     Scanner input;
 
+    /**
+	 * Constructs a scanner that is used to read the input.
+	 * @param scanner the scanner.
+	 */
     public InputHelper(Scanner scanner){
         input = scanner;
     }
 
+    /**
+	 * Checks if player answers prompt with y, Y, n, or N.
+	 * @param prompt the message given to player.
+     * @return true if input starts with y, Y, n, or N.
+	 */
     public boolean getYesNoInput(String prompt){
         System.out.println(prompt);
         String userInput = input.nextLine().substring(0, 1).toLowerCase();
@@ -22,7 +31,14 @@ public class InputHelper {
         }
         return false;
     }
-
+    
+    /**
+	 * Checks if player answers prompt with proper integer.
+	 * @param prompt the message given to player.
+     * @param min the minimum value of the integer.
+     * @param max the maximum value of the integer.
+     * @return userInput if input is larger than or equal to min & less than or equal to max.
+	 */
     public int getIntegerInput(String prompt, int min, int max){
         System.out.println(prompt);
         int userInput = input.nextInt();
